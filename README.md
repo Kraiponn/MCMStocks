4925dcde6bf681460183a301416da68d039412f6
kraiponn <kraipon.na10@gmail.com>
2021-01-12 22:40:43 +0700

    Initial project

---
+++ README.md
@@ -0,0 +1,15 @@
+# CMStock Workshop
+> Mini workshop for product stock
+
+## Tools
+```
+ - Kotlin
+ - easypref : sharedpreferenes
+```
+
+## Demo
+```
+  - Version: 1.0.0
+  - Author: Kraipon Najaroon
+  - License: ISC
+```
---
+++ app/src/main/java/com/codemakerlab/mcmstocks/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codemakerlab.mcmstocks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
